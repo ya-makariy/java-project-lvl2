8154b616092280c00a7f73b234267f1c883e6675
m.balashov <m.balashov@crpt.ru>
2022-02-25 08:04:07 +0300

    add Parameters and Option

--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -1,10 +1,22 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@CommandLine.Command(name = "app", mixinStandardHelpOptions = true, version = "app 1.0",
-        description = "Print string Hello World!")
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "app 1.0",
+        description = "Compares two configuration files and shows a difference.")
 public class Differ implements Runnable {
+
+    @Parameters(index = "0", description = "path to first file")
+    String filepath1;
+
+    @Parameters(index = "1", description = "path to second file")
+    String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
 
     @Override
     public void run() {
